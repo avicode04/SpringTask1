579cd9f489e3414ce4ba391d60fb28ecebb01b33
avinash <avinashchandrakar04@gmail.com>
2019-09-24 17:19:38 +0530

    finalcommit

--- src/main/java/com/stackroute/FilmIndustry.java
+++ src/main/java/com/stackroute/FilmIndustry.java
@@ -7,8 +7,12 @@ import org.springframework.core.io.ClassPathResource;
 public class FilmIndustry {
     public static void main(String args[]){
         XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movie =  factory.getBean("movie",Movie.class);
-        System.out.println("Movie");
-        movie.getActor();
+        Movie movie1 =  factory.getBean("movieA",Movie.class);
+        System.out.println("Movie Scripted...");
+        Movie movie2 = factory.getBean("movieB",Movie.class);
+        System.out.println("Movie Scripted...");
+        movie1.getActor();
+        movie2.getActor();
+        System.out.println(movie1==movie2);
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,14 +1,17 @@
 package com.stackroute.domain;
 
 public class Movie {
-    private Actor actor;
+    private Actor actor1;
+    private Actor actor2;
+    private Actor actor3;
 
-    public void setActor(Actor actor) {
-        System.out.println("Movie Scripted...");
-        this.actor = actor;
+    public Movie(Actor actor1, Actor actor2, Actor actor3) {
+        this.actor1 = actor1;
+        this.actor2 = actor2;
+        this.actor3 = actor3;
     }
 
     public void getActor(){
-        actor.display();
+        actor1.display();actor2.display();actor3.display();
     }
 }
